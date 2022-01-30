c866fc1bc94f8cfd65bcac794cd022bbd3ec6c88
Robert <AQ_ROBY@YAHOO.COM>
2022-01-30 23:06:48 +0200

    Add post model

--- website/models/Post.py
+++ website/models/Post.py
@@ -1,5 +1,8 @@
-class post:
-    def __init__(self, id, title, content):
+class Post:
+    def __init__(self, id, title, content, owner, created_at, modified_at):
         self.id = id
         self.title = title
         self.content = content
+        self.owner = owner
+        self.created_at = created_at
+        self.modified_at = modified_at
--- website/views.py
+++ website/views.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template
-from .models import Post
-from .repository import repository
+from .models.Post import Post
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 @views.route("/index")
 def index():
-    content = ["Alex", "Mariana"]
-    x = repository()
+    x = Post(1, "Post 1", "This is a post", "Marian", "01-02-2020", "01-02-2020")
+    y = Post(2, "Post 2", "This is another post", "Ion", "07-02-2021", "07-02-2021")
+    li = [x, y]
     
-    return render_template("index.html", content=content)
+    return render_template("index.html", content=li)
