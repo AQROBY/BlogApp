15205753c4aae4c5a53b589ccce746093cb40fd4
Robert <AQ_ROBY@YAHOO.COM>
2022-02-11 01:17:22 +0200

    Add conftest and functional tests for index

--- tests/functional/test_index.py
+++ tests/functional/test_index.py
@@ -1,1 +1,10 @@
+from tests.conftest import app
+from flask import url_for
 
+def test_index(client, app):
+    response = client.get('/')
+    assert response.status_code == 302
+
+def test_index_redirect(client, app):
+    response = client.get('/')
+    assert '/posts' in response.location
